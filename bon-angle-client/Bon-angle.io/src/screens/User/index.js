9e3a2f015b9bcfc1ea9236b452622b79858e8a46
phimth <tphiman@gmail.com>
2019-09-10 20:08:23 +0200

    bonnes vacances

--- bon-angle-client/Bon-angle.io/src/screens/User/index.js
+++ bon-angle-client/Bon-angle.io/src/screens/User/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView,Dimensions } from 'react-native';
 import users from './../User/users';
 import items from './../List/items';
 import Item from './../User/Item';
+import { Ionicons,AntDesign,Entypo,MaterialIcons } from '@expo/vector-icons';
+import { Camera } from 'expo-camera';
 
 export default class User extends Component{
     constructor(props){
@@ -21,11 +23,18 @@ export default class User extends Component{
         this.setState({items:products})
     }
 
+    add(){
+
+    }
+
     render(){
         const user = users.find((user) => user.id == '2daf302b-ecb1-4704-9d87-fdeb4c7abd9c');
+        const screenWidth = Math.round(Dimensions.get('window').width);
+        const screenHeight = Math.round(Dimensions.get('window').height);
 
         return(
             <SafeAreaView>
+                <Ionicons name="ios-add-circle" size={24} color="green" style={{position:'absolute',right:20,top:20}} onPress={()=> this.add()}/>
                 <ScrollView>
             <Text style={{textAlign:'center'}}>Hi {user.firstName}</Text>
             <Text style={{textAlign:'center'}}>Here are the products you are selling</Text>
